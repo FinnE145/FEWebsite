fbe36dfa4c1f1ed7dfc5cda80e280e144fbfae3e
FinnE145 <finn.emmerson.12@gmail.com>
2024-08-04 22:23:15 -0400

    add test funcs to app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def index():
+    return "index"
+
+@app.route("/test")
+def test():
+    return "test"
 
 if __name__ == "__main__":
     app.run()
